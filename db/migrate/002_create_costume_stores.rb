f443fc664f997c2e6f78f448bb21e6d40a836027
Yingqi Chen <chanwk18@hotmail.com>
2019-06-03 20:45:46 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,10 +5,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employee_count
+      t.integer :num_of_employees
       t.boolean :open
-      t.datetime :opening
-      t.datetime :closing
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
 
   end
