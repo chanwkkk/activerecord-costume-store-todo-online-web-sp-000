18a07eb78e9a33322110f3c3751e5cfe3f599c07
Yingqi Chen <chanwk18@hotmail.com>
2019-06-03 20:28:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,10 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.integer :size
+      
   end
 end
