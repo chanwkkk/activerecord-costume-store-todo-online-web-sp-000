c09ae03f6ac2ee719f9a9f7d977123fa329415ee
Yingqi Chen <chanwk18@hotmail.com>
2019-06-03 20:43:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,24 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
+      t.string :name
+      t.text :location
+      t.string :theme
+      t.float :price
+      t.boolean :open
+      t.datetime :opening
+      t.datetime :closing
+      t.text :description
 
     end
   end
 end
+
+name
+location
+theme
+price
+whether they're family friendly or not
+opening date
+closing date
+long description
